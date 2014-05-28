8c9655e99cb8daf707ce2b31e82f8182b21b9e7f
Yousuf Fauzan <yousuf@zite.com>
2014-05-28 11:34:29 -0700

    initial commit

---
+++ tidy/src/version.h
@@ -0,0 +1,14 @@
+/* version information
+
+  (c) 2006 (W3C) MIT, ERCIM, Keio University
+  See tidy.h for the copyright notice.
+
+  CVS Info :
+
+    $Author: arnaud02 $ 
+    $Date: 2006/07/04 17:10:35 $ 
+    $Revision: 1.5 $ 
+
+*/
+
+static const char release_date[] = "4 July 2006";
---
+++ tidylib-20070328/src/version.h
@@ -0,0 +1,14 @@
+/* version information
+
+  (c) 2007 (W3C) MIT, ERCIM, Keio University
+  See tidy.h for the copyright notice.
+
+  CVS Info :
+
+    $Author: arnaud02 $ 
+    $Date: 2007/03/21 16:12:06 $ 
+    $Revision: 1.19 $ 
+
+*/
+
+static const char TY_(release_date)[] = "21 March 2007";
